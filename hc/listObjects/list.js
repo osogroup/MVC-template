2d006a53d3bcb7e18beb6b0233dec95505a9a8e7
andersonriley <andersonriley28@gmail.com>
2021-05-19 11:22:08 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -127,7 +127,7 @@ const createHTML = async () => {
   //   + '<a class="headerLinks" href="#">Task</a>'
   //   + '<a class="headerLinks" href="#">Deliverable</a>'
   //   + '</p></div>';
-    var listHTML +=  '<div class="col-12" id="TitleOfList">OAS Object List</div>';
+    var listHTML =  '<div class="col-12" id="TitleOfList">OAS Object List</div>';
     listHTML +=  '</div><div class="row" id="hacker-list"><div class="col-12">';
 
     //------------------------------------------------------- Input Boxes ------------------------------------------------------------------
