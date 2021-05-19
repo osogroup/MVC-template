101d80f665fe5ba633eed282fc1d1b55f8db4bf4
andersonriley <andersonriley28@gmail.com>
2021-05-19 09:13:34 -0500

    Making the variable that will contain the header and all JSON stuff once its finished

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -117,7 +117,18 @@ var listHTML = '<div id="contacts" class="container>';
                  + '<a class="headerLinks" href="#">Tag</a>'
                  + '<a class="headerLinks" href="#">Task</a>'
                  + '<a class="headerLinks" href="#">Deliverable</a>'
-               + '</p>';
+               + '</p></div>';
+    listHTML +=  '<div class="col-12" id="TitleOfList">OAS'+type+'Object List</div>'
+    for (const [kee, va] of Object.entries(objVars.create)) {
+      listHTML += '<div class="col-3">'
+                  + '<input type="text" id="'+va+'-field" placeholder="'+va+'" />'
+                + '</div>'
+    }
+      listHTML +=  '<div class=col-1">'
+                     '<button id="add-btn">Add</button>'
+                     '<button id="edit-btn">Edit</button>'
+                   '</div>'
+                 '</div>'
 
 
 // let requestURL = 'https://github.com/osogroup/MVC-template/blob/main/hc/tempData/listOfObjects.json';
