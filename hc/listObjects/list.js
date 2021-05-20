0a1c84b418505f4f00e86c90230a8cabb26f9dc2
andersonriley <andersonriley28@gmail.com>
2021-05-20 07:44:35 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -163,8 +163,10 @@ const createHTML = async () => {
 // ----------------------------------------------------- The Search Box ----------------------------------------------------------------
 
     var searchVariable = '<div class="col-12">'
-                         + '<div class="col-4">'
-                           + '<input class="search" placeholder="Search"/>'
+                         + '<div class="row">'
+                           + '<div class="col-4">'
+                             + '<input class="search" placeholder="Search"/>'
+                           + '</div>'
                          + '</div>'
                        + '</div>';
     $('#searchBar').append(searchVariable);
