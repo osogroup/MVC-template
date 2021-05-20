f0e83e34d3402bf13049f4c1375ad4038edb9aab
andersonriley <andersonriley28@gmail.com>
2021-05-20 09:56:38 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -59,7 +59,7 @@ $(document).ready(function(){
       estField = $('#est-field'),
       addBtn = $('#add-btn'),
       editBtn = $('#edit-btn').hide(),
-      removeBtns = $(removeBtns.selector),
+      removeBtns = $('.remove-item-btn'),
       editBtns = $('.edit-item-btn');
     // Needed to add new buttons to jQuery-extended object
 
