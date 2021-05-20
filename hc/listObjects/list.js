f20ed76fcfa37b6a250f408b6ec06b2a55cbfde2
andersonriley <andersonriley28@gmail.com>
2021-05-20 12:23:10 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -224,7 +224,7 @@ const createHTML = async () => {
       console.log("prop is: ", prop);
       console.log("access is: ", access);
       // console.log("prop.access is: ", prop.access);
-      top += '<div class="col-'+col+'">'+prop.access+'</div>';
+      top += '<div class="col-'+col+'">'+prop[access]+'</div>';
 
     }
 
