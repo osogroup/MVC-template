8c568757df710017a07ace68bebe05ce336add60
andersonriley <andersonriley28@gmail.com>
2021-05-20 14:10:36 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -146,7 +146,7 @@ const createHTML = async () => {
 
   for (const [key, value] of Object.entries(data.task)) {
     for (const [first, last] of Object.entries(value)) {
-      console.log("THIS IS WHAT I WANT TO SEE ", last);
+      // console.log("this is last ", last);
       inputThings += '<div class="col-3">'
                      + '<input type="text" id="'+first+'-field" placeholder="'+first+'" />'
                    + '</div>';
