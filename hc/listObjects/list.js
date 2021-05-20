7ba9184b9a528862624982dc172c362fb8c40576
andersonriley <andersonriley28@gmail.com>
2021-05-20 15:06:06 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -183,11 +183,10 @@ const createHTML = async () => {
     console.log("val is: ", val);
     var top = '<h1>'+key+'</h1>';
     top += '<div class="row">';
-    var listConfig = config[key];
-    var listData = data[key];
-    var col = 12/(listConfig.list.length);
-    console.log(key+" configs ", listConfig);
-    for (const [first, last] of Object.entries(listConfig.list)) {
+    var listData = config[key];
+    var col = 12/(listData.list.length);
+    console.log(key+" configs ", listData);
+    for (const [first, last] of Object.entries(listData.list)) {
       console.log("This is first ", first);
       console.log("This is last ", last);
 
@@ -196,7 +195,7 @@ const createHTML = async () => {
                    + '</div>';
     }
 
-    for (const [second, secondToLast] of Object.entries(listConfig.list)) {
+    for (const [second, secondToLast] of Object.entries(listData.list)) {
       console.log("This is second ", second);
       console.log("This is secondToLast ", secondToLast);
       top += '<div class="col-'+col+'">'+secondToLast+'</div>';
