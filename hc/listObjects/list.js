90a8a0c6b6a190b03434f00e66c06be6deb48a9d
andersonriley <andersonriley28@gmail.com>
2021-05-20 11:26:34 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -208,9 +208,9 @@ const createHTML = async () => {
   // Ex. all the id's and names of projects that you can scroll through, and eventually, remove.
   for (const [key, val] of Object.entries(data)) {
     var top = '<h1>'+key+'</h1>';
-    var list = config[key];
-    console.log(key+" configs ", list);
-    for (const [first, last] of Object.entries(value)) {
+    var listData = config[key];
+    console.log(key+" configs ", listData);
+    for (const [first, last] of Object.entries(listData.list)) {
       inputThings += '<div class="col-3">'
                      + '<input type="text" id="'+first+'-field" placeholder="'+first+'" />'
                    + '</div>';
