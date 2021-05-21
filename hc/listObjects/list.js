264dbaa13b259c1e727659b996a721f88b0634fd
andersonriley <andersonriley28@gmail.com>
2021-05-21 14:17:38 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -208,10 +208,8 @@ const createHTML = async () => {
 
 
         var searchVariable =  '<div class="row">'
-                              + '<div class="col-12">'
-                                + '<div class="col-4">'
-                                  + '<input class="search" placeholder="Search"/>'
-                                + '</div>'
+                              + '<div class="col-4">'
+                                + '<input class="search" placeholder="Search"/>'
                               + '</div>'
                             + '</div>';
         
