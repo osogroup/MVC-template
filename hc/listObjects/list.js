93e5c0e19ffa43675c141b95c1b4b669c969f3b6
andersonriley <andersonriley28@gmail.com>
2021-05-24 08:26:25 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -322,7 +322,7 @@ const createHTML = async () => {
     }
 
     addBtn.click(function() {
-      console.log("Entering addBtn.click(function() {...");
+      console.log("Entering addBtn.click(function() {...}");
       contactList.add({
         id: Math.floor(Math.random()*110000),
         name: nameField.val(),
