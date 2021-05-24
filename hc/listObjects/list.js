f589aaf64a2814f787b92d85020efc0466ef9894
andersonriley <andersonriley28@gmail.com>
2021-05-24 11:27:03 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -329,8 +329,8 @@ const createHTML = async () => {
       // removes a row of data from the container
       removeBtns.click(function() {
         console.log("Entering remove function", this);
-        var itemId = $(this).parent().parent().parent().parent();
-        console.log($(this).parent().parent().parent().parent());
+        var itemId = $(this).parent().parent().parent().parent().attr('id');
+        console.log($(this).parent().parent().parent().parent().attr('class'));
         console.log("this is the item id " + itemId);
         obj.remove('id', itemId);
       });
