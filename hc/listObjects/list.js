6479871401a75279fc0978312b85c8aefab218fc
andersonriley <andersonriley28@gmail.com>
2021-05-24 07:52:23 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -274,9 +274,9 @@ const createHTML = async () => {
       options.valueNames.push(sortValue);
     }
 
-    var containerList = new List(key+'Container', options);
-    
     refreshCallbacks(containerList);
+
+    var containerList = new List(key+'Container', options);
     
     // closing each id=hacker-list div
     $('#tableHeadersandItems').append('</div>');
