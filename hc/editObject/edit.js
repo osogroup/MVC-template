46d059e4840b722ba46d0103614d127c2afe380c
andersonriley <andersonriley28@gmail.com>
2021-05-25 10:31:54 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -1,4 +1,5 @@
-// get type and itemid from URL (objType and objItemID) ex. task and 0
+// get information from URL (objType, objItemID, objItemName objItemDesc, and objItemTags) 
+// ex. task, 0, COI: Design Wireframes, These tasks will result in a user interface to create an object item, and 0
 
 // function to create editObject HTML
 
