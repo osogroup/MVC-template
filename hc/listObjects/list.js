0ed96230b44e249d6d60ce3594f37812435e2227
andersonriley <andersonriley28@gmail.com>
2021-05-25 09:24:23 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -238,7 +238,6 @@ const createHTML = async () => {
       // getting item attributes
       // console.log("THIS IS MY FOCUS", data[objType]);
       for (const [SLKey, SLValue] of Object.entries(data[objType])) {
-
         // creating the object item rows
         objItems += '<div class="row">';
 
@@ -256,7 +255,7 @@ const createHTML = async () => {
 
         // closing object item rows
         objItems += '<div class=col-1>'
-                    + '<a href="/hc/editObject/?type='+objType+'&itemid='+data[objType][id]+'"><button class="edit-item-btn"> Edit </button>'
+                    + '<a href="/hc/editObject/?type='+objType+'&itemid='+SLValue.id+'"><button class="edit-item-btn"> Edit </button>'
                   + '</div>'
                   + '<div class=col-1>'
                     + '<button class="remove-item-btn"> Remove </button>'
