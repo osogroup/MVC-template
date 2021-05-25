ea2928491d0485d71a3923e302a15d3fa26bfe58
andersonriley <andersonriley28@gmail.com>
2021-05-25 13:51:02 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -82,14 +82,14 @@ const outputHTML = async () => {
                    + '<div class="col-2"><input class="col-12" type="text" placeholder="'+objItemDesc+'"></div>'
 
                    // creating item area
-                   + '<div class="col-2"><div class="col-12">'+objItemID+'</div></div>'
+                   + '<div class="col-2"><div class="col-12">'+objItemStatus+'</div></div>'
 
                    //  creating item input box
                    + '<div class="col-2"><input class="col-12" type="text" placeholder="'+objItemTags+'"></div>'
 
                    //  creating item areas
-                   + '<div class="col-2"><div class="col-12">'+objItemID+'</div></div>'
-                   + '<div class="col-1"><div class="col-12">'+objItemID+'</div></div>'
+                   + '<div class="col-2"><div class="col-12">'+objItemDue+'</div></div>'
+                   + '<div class="col-1"><div class="col-12">'+objItemEstEff+'</div></div>'
 
                  // closing item row
                  + '</div>';
