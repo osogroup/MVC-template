6fbd827dc1fa2f1ee214f05f7172841e810d8efa
andersonriley <andersonriley28@gmail.com>
2021-05-25 08:52:44 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -22,18 +22,18 @@ const createHTML = async () => {
 
   var headerHTML = '<div id="contacts">'
   + '<div class="row">'
-  + '<div class="col-12">'
-      + '<p id="header">'
+    + '<p id="header">'
       + '<img src="https://brandmark.io/logo-rank/random/pepsi.png" alt="LogoImage" width="80">';
   
   // creating the links for the header
   for(const [headerKey, headerValue] of Object.entries(data)) {
-  // headerHTML +=        '<a class="headerLinks" href="/hc/listObjects/?type='+headerKey+'">'+headerKey+'</a>';
     headerHTML += '<a class="headerLinks" href="?type='+headerKey+'">'+headerKey+'</a>';
   }
   
-  headerHTML  +=  '</p>'
-              + '</div>';
+  // closing header and col-12 
+  headerHTML += '</p>';
+
+  // making a propt scenario where there is no object type selected
   if (objType == null) {
     headerHTML += '<h1>Select Item from Header to Create Item...</h1>';
   }
