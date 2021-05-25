bb9c4e5623215ab5069caaaa1d1946c2f957f065
andersonriley <andersonriley28@gmail.com>
2021-05-25 10:36:36 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -1,8 +1,39 @@
 // get information from URL (objType, objItemID, objItemName, objItemDesc, and objItemTags) 
 // ex. task, 0, COI: Design Wireframes, These tasks will result in a user interface to create an object item, and 0
 
-// function to create editObject HTML
 
 // pull in data from objectList.JSON and configuration JSON file 
+const configData = async () => {
+  var listConfig = await fetch("../tempData/objectConfig.json").then(response=>{return response.json();});
+  // console.log("configData output ", listConfig);
+  return listConfig;
+}
+  
+  // gets all the Objects/Arrays from listOfObjects.json and returns them
+const tempData = async () => {
+  var objVars = await fetch("../tempData/listOfObjects.json").then(response=>{return response.json();});
+  // console.log("tempData output ", objVars);
+  return objVars;
+}
 
-// use itemid to get data out of the JSON file that create and fill in the input boxes
+// function to create editObject HTML
+const outputHTML = async () => {
+  console.log("Entering outputHTML()");
+  var data = await tempData();
+  var config = await configData();
+
+  
+  // use itemid to get data out of the JSON file that create and fill in the input boxes
+
+
+
+
+
+
+
+
+
+
+
+  console.log("Exiting outputHTML()");
+}
