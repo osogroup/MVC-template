8fd749268ec3de37a84c13c44024e64ec6828518
andersonriley <andersonriley28@gmail.com>
2021-05-25 09:44:14 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -1,1 +1,7 @@
+// get type and itemid from URL
 
+// function to create editObject HTML
+
+// pull in data from objectList.JSON and configuration JSON file 
+
+// use itemid to get data out of the JSON file that create and fill in the input boxes
