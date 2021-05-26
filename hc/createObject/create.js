506f6cd5185c66917118b1b092f8ee8c12219fbb
andersonriley <andersonriley28@gmail.com>
2021-05-26 15:35:57 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -72,7 +72,6 @@ function doTheHTML() {
   localStorage.setItem("name", "Riley Anderson");
   console.log(localStorage);
 
-  for(const[] of Object.entries())
   var inputBox = '<input class="col-12" id="input" type="textarea" value="'+localStorage.getItem("")+'" oninput="localStorage.setItem("", this.value)">';
   $('#everything').append(inputBox);
 
