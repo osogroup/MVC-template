cdead460995514e5e91b899d1be530547ac428e7
andersonriley <andersonriley28@gmail.com>
2021-05-26 12:04:08 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -82,7 +82,7 @@ const outputHTML = async () => {
     if (configTypeData.editable.includes(headerKey) == true)
     {
       // making object item an input textbox
-      itemHeader += '<br><input class="col-12" id="input'+headerKey+'" type="textarea" value="'+headerValue+'" onchange="addLocal('+headerValue+', this.value)">';
+      itemHeader += '<br><input class="col-12" id="input'+headerKey+'" type="textarea" value="'+headerValue+'" onchange="addLocal('+headerKey+', this.value)">';
     }
     else
     {
