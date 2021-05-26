75e1693f564c4149ca81fcc868898672b56be0a0
andersonriley <andersonriley28@gmail.com>
2021-05-26 14:46:07 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -61,23 +61,24 @@ const createHTML = async () => {
 
 
 
+
+
+function addLocal(localKey, localValue) {
+  console.log("localKey ", localKey);
+  console.log("localValue ", localValue);
+  localStorage.setItem(localKey, localValue);
+  console.log(localStorage);
+}
+
+
 function doTheHTML() {
-  
+
   var local = "name";
   // localStorage.clear();
   // localStorage.setItem("name", "Riley Anderson");
   console.log(localStorage);
-  
+
   var inputBox = '<input class="col-12" id="input" type="textarea" value="'+localStorage.getItem("")+'" oninput="addLocal('+local+', this.value);">';
   $('#everything').append(inputBox);
-  
-  
-  addLocal(localKey, localValue) {
-    console.log("localKey ", localKey);
-    console.log("localValue ", localValue);
-    localStorage.setItem(localKey, localValue);
-    console.log(localStorage);
-  }
+
 }
-
-
