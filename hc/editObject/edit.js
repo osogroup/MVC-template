bbc46fcdf38c437b141d4c646cd57e72e2b51882
andersonriley <andersonriley28@gmail.com>
2021-05-26 11:22:05 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -62,6 +62,7 @@ const outputHTML = async () => {
 
   // This is INCORRECT, using the ID is only going to work when the ID is equal to the position in the array
   for (const [headerKey, headerValue] of Object.entries(objTypeData[objItemid])) {
+    localStorage.setItem(headerKey, headerValue);
     // console.log("This is headerKey: ", headerKey);
     // console.log("This is headerValue: ", headerValue);
     
@@ -88,6 +89,7 @@ const outputHTML = async () => {
     // closing each object item column
     itemHeader += '</div>';
   }
+  console.log(localStorage);
   // closing item header row
   itemHeader += '</div>';
   $('#HTMLoutput').append(itemHeader);
