d3819d477cad43d78578486d2ae97d6116db0cfd
andersonriley <andersonriley28@gmail.com>
2021-05-27 11:58:13 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -110,6 +110,7 @@ function doTheHTML() {
 
 const doTheStringify = async () => {
   var data = await tempData();
+  var objTypeData = data[objType];
 
   // example localStorage(task_0, '{"id":0,"name":"COI: Design Wireframes","description":"These tasks will result in a user interface to create an object item.","status":"in-progress","tags":[0],"dueDate":"5/15/2021","estEff":10}');
 
