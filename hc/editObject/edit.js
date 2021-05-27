29b0bc5db9fea1df369b2634fcfe321dfd18536c
andersonriley <andersonriley28@gmail.com>
2021-05-27 08:09:28 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -63,8 +63,8 @@ const outputHTML = async () => {
   // This is INCORRECT, using the ID is only going to work when the ID is equal to the position in the array
   for (const [headerKey, headerValue] of Object.entries(objTypeData[objItemid])) {
     localStorage.setItem(headerKey, headerValue);
-    // console.log("This is headerKey: ", headerKey);
-    // console.log("This is headerValue: ", headerValue);
+    console.log("This is headerKey: ", headerKey);
+    console.log("This is headerValue: ", headerValue);
     
     // creating each object item column
     itemHeader += '<div class="col-4 minHeight">'
