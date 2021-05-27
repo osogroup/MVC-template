1a35c07f01756de37406b96ea1dfca5a67c62bae
andersonriley <andersonriley28@gmail.com>
2021-05-27 07:38:45 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -88,7 +88,7 @@ function doTheHTML() {
   console.log(localStorage);
   var inputBox = '<br><br><br><div class="row"><div class="col-8">Name</div>'
                  + '<input class="col-8" id="inputBoxName" type="textarea" placeholder="name" value="Riley Anderson" oninput="anyChange(this.placeholder)"></div>';
-     inputBox += '<br><br><br><div class="row"><div class="col-8">Age</div>'
+     inputBox += '<div class="row"><div class="col-8">Age</div>'
                  + '<input class="col-8" id="inputBoxAge" type="textarea" placeholder="age" value="22" oninput="anyChange(this.placeholder)"></div>';
 
   $('#everything').append(inputBox);
