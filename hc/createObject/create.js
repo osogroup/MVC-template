2ea3d5b830afed5b19349d86dd05bd4becae8545
andersonriley <andersonriley28@gmail.com>
2021-05-27 11:34:03 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -148,8 +148,8 @@ const doTheStringify = async () => {
 
     //   if object id is equal to the one im searching for
     for(const[repositoryKey, repositoryValue] of Object.entries(data[objType])) {
-      console.log("This is repositoryKey: ", repositoryKey);
-      console.log("This is repositoryValue: ", repositoryValue);
+      console.log("This is repositoryKey: ", repositoryKey); // this is the id number 
+      console.log("This is repositoryValue: ", repositoryValue); // this is the object
       // if (repositoryItem[id] == objectKeys) {
       //   objItem = repositoryItem;
       //   console.log("This is objItem: ", objItem);
