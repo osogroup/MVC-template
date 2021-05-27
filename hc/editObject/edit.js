2e1bd230c031734535b10816d73675e7f0a989f9
andersonriley <andersonriley28@gmail.com>
2021-05-27 09:02:26 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -73,10 +73,8 @@ const outputHTML = async () => {
   // This is INCORRECT, using the ID is only going to work when the ID is equal to the position in the array
   for (const [headerKey, headerValue] of Object.entries(objTypeData[objItemid])) {
 
-    console.log("This is the objTypeData[objItemid].id .. ", objTypeData[objItemid].id);
-    if (localStorage.getItem(id) != objTypeData[objItemid].id) {
-      localStorage.setItem(headerKey, headerValue);
-    }
+    // TOGGLE THIS TO PULL INFO FROM THE LIST PAGE*************************
+    localStorage.setItem(headerKey, headerValue);
     console.log("This is headerKey: ", headerKey);
     console.log("This is headerValue: ", headerValue);
     
