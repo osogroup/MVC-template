d424771261f4a16502ba20540c3b0a935e772941
andersonriley <andersonriley28@gmail.com>
2021-05-28 11:22:40 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -94,8 +94,7 @@ const createHTML = async () => {
                 + '<option value="a">A</option>'
                 + '<option value="b">B</option>'
                 + '<option value="c">C</option>'
-              + '</select>'
-              + '<button onClick="getSelectedItems()">Add</button>';
+              + '</select>';
 
 
   // HTMLoutput += '<button onclick="generateID()">Run generateID()</button>';
