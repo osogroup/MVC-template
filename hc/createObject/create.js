3a863c47ac5ca76252113c0b30f39b0a4340eae5
andersonriley <andersonriley28@gmail.com>
2021-05-28 14:49:14 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -136,7 +136,7 @@ const createHTML = async () => {
   }
 
   removeVar.click(function() {
-    console.log("Entering removeField function", this);
+    console.log("Entering removeVar function", this);
     // var listContainer = $(this).parent().parent().parent().parent().attr('id');
     // console.log("This is listContainer: ", listContainer);
     // var itemId = $(this).parent()find('.display').text();
