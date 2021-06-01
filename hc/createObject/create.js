ac6672d1c86e8b46b24d3f973df95c4caf4efa3b
andersonriley <andersonriley28@gmail.com>
2021-06-01 09:25:25 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -141,7 +141,7 @@ const createHTML = async () => {
       for (const [fieldKey, fieldValue] of Object.entries(arrayFields)) {
         // console.log("This is fieldKey: ", fieldKey); // 0,1,2
         // console.log("This is fieldValue: ", fieldValue); // peter, john, james
-        HTMLelement += '<div class="row" id="'+objType+fieldKey+fieldValue+'">'+fieldValue+' <input type="button" class="remvBtn" onClick="removeFunction(\''+elementValue+'\')" value="-"></div>';
+        HTMLelement += '<div class="row" id="'+objType+fieldKey+fieldValue+'"><div class="col-4">'+fieldValue+'</div><input type="button" class="remvBtn" onClick="removeFunction(\''+elementValue+'\')" value="-"></div>';
 
       }
       console.log("This is arrayFields: ", arrayFields);
