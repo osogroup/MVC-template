429def6f5fe8ce7f4959864d6a42810be832bfba
andersonriley <andersonriley28@gmail.com>
2021-06-01 15:28:21 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -182,7 +182,7 @@ const createHTML = async () => {
 function removeFunction(val) {
   console.log("Entered removeFunction...");
   console.log("This is the parameter 'val': ", val);
-  arrayFields.splice(i, 1);
+  arrayFields.splice(val, 1);
   localStorage.removeItem(val);
   // location.reload();
 }
