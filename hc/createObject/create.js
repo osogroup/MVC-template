529d77ecba68f1dfaaffbad654867b046def382e
andersonriley <andersonriley28@gmail.com>
2021-06-01 12:55:05 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -133,7 +133,7 @@ const createHTML = async () => {
 
     // HTMLoutput += '<button onclick="generateID()">Run generateID()</button>';
     
-    $('#arraySpot').innerHTML = HTMLarray;
+  $('#arraySpot').append(HTMLarray);
 
   }
 
