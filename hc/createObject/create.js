44965bce5391ed1b3096f58de729dee082765f63
andersonriley <andersonriley28@gmail.com>
2021-06-01 14:13:03 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -106,11 +106,11 @@ const createHTML = async () => {
 
   function displayArray() {
 
-    var element = document.getElementById('arraySpot');
-    console.log("This is element: ", element);
-    element.parentNode.removeChild(element);
-    var newElement = document.createElement('div');
-    newElement.setAttribute('id', 'arraySpot');
+    // var element = document.getElementById('arraySpot');
+    // console.log("This is element: ", element);
+    // element.parentNode.removeChild(element);
+    // var newElement = document.createElement('div');
+    // newElement.setAttribute('id', 'arraySpot');
 
     var HTMLarray = '<form action="#" method="post" id="demoForm" class="demoForm">'
                   + '<fieldset>'
