f6b6c24a5565bc12afda2746f7f256ad63f3dfca
andersonriley <andersonriley28@gmail.com>
2021-06-01 12:29:26 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -106,8 +106,7 @@ const createHTML = async () => {
                   + '<legend>Get Text of Selected Option</legend>'
 
   // showing all the items in the arrayFields array
-  var parsedData = JSON.parse(localStorage);
-  for (const[arrayKey, arrayValue] of Object.entries(parsedData)) {
+  for (const[arrayKey, arrayValue] of Object.entries(localStorage)) {
     // console.log("This is arrayKey: ", arrayKey); // (0, 1, 2)
     console.log("This is arrayValue: ", arrayValue); // (peter, james, john)
     HTMLoutput +=     '<div class="row"><div class="col-4">'+arrayValue+'</div><div class="col-1"><input type="button" class="remvBtn" value="-"></div></div>';
