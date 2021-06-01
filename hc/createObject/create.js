c782506a16651934a579f3f1d1996afd6f27e161
andersonriley <andersonriley28@gmail.com>
2021-06-01 08:14:03 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -142,12 +142,12 @@ const createHTML = async () => {
 
 }
 
-  function removeFunction(val) {
-    console.log("Entered removeFunction...");
-    console.log("This is the parameter 'val': ", val);
-    var removeVal = arrayFields.indexOf(val);
-    console.log("This is removeVal: ", removeVal);
-  }
+function removeFunction(val) {
+  console.log("Entered removeFunction...");
+  console.log("This is the parameter 'val': ", val);
+  var removeVal = arrayFields.indexOf(val);
+  console.log("This is removeVal: ", removeVal);
+}
 
 
 
