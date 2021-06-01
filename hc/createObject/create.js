647f308c3c7f58ac08a05810174d3f72886ea00e
andersonriley <andersonriley28@gmail.com>
2021-06-01 07:24:40 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -129,7 +129,7 @@ const createHTML = async () => {
       // access text property of selected option
       element.value = select.options[select.selectedIndex].text;
       arrayFields.push(element.value);
-      var HTMLelement = '<div class="row">'+element.value+' <input type="button" onclick="removeFunction("'+element.value+'")" value="-"></div>';
+      var HTMLelement = '<div class="row">'+element.value+' <input type="button" onclick="removeFunction('+element.value+')" value="-"></div>';
       console.log("This is arrayFields: ", arrayFields);
       $('#display').append(HTMLelement);
     }
