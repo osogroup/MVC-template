4ca81012399707f39db8134cd15b04f46daaeb3c
andersonriley <andersonriley28@gmail.com>
2021-06-01 15:04:25 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -116,7 +116,7 @@ const createHTML = async () => {
     HTMLarray += '<form action="#" method="post" class="demoForm">'
                   + '<fieldset>'
                     + '<legend>Get Text of Selected Option</legend>'
-                    + '<div id="appendTo"></div>';
+                    + '<div id="appendTo" class="row"></div>';
 
     // showing all the items in the arrayFields array
     for (const[arrayKey, arrayValue] of Object.entries(arrayFields)) {
