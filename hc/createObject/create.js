9a586d9819b9933f246888f3a70d0b8cd7b0a96a
andersonriley <andersonriley28@gmail.com>
2021-06-02 09:20:21 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -100,7 +100,6 @@ const createHTML = async () => {
   $('#everything').append(HTMLoutput);
 
   var HTMLarray = '';
-  displayArray();
 
 
   // ---------------------------------------------- Display Array ----------------------------------------------
