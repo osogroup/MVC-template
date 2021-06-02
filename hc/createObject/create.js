a74a53d02903201f669a14ce1edca74c814985af
andersonriley <andersonriley28@gmail.com>
2021-06-02 07:31:08 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -114,6 +114,7 @@ const createHTML = async () => {
 
     // showing all the items in the arrayFields array
     for (const[arrayKey, arrayValue] of Object.entries(arrayFields)) {
+      i = 0;
       // console.log("This is arrayKey: ", arrayKey); // (0, 1, 2)
       // console.log("This is arrayValue: ", arrayValue); // (peter, james, john)
       HTMLarray +=     '<div class="row"><div class="col-6">'+arrayValue+'</div><div class="col-1"><input type="button" class="remvBtn" value="-" onclick="removeFunction('+arrayKey+')></div></div>';
