2d9d0bf4ee334204f241e902b2da166831f10d15
andersonriley <andersonriley28@gmail.com>
2021-06-02 12:29:33 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -267,15 +267,14 @@ function createObjectList() {
                   + '</div>';
 
   for (const[objectKey, objectValue] of Object.entries(objectFields)) {
-    // (arrayKey: 0, 1, 2)
-    // (arrayValue: peter, james, john)
-    HTMLobjectValues +=   '<div class="row"><div class="col-6">'+objectValue+'</div><div class="col-1"><input type="button" id="remvBtn'+i+'" value="-" onclick="removeFunction('+i+')"></div></div>';
+    // (objectKey: option 1, option 2, option 3)
+    HTMLobjectValues += '<div class="row"><div class="col-6">'+objectValue+'</div><div class="col-1"><input type="button" id="remvBtn'+i+'" value="-" onclick="removeFunction('+i+')"></div></div>';
     // counts up the indices if there's any preset values in the array (uncommon)
     i++;
   }
 
 
-  outputHTML +=     '<br><select>';
+  outputHTML +=     '<br><select id="objScripts" name="objScripts">';
 
   for (const [fillKey, fillValue] of Object.entries(fillFields)) {
   outputHTML +=        '<option value="'+fillKey+'">'+fillKey+'</option>';
