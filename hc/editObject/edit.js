c6813686c9dd0b8971c7a17c519a7f4aa8b9a2d9
andersonriley <andersonriley28@gmail.com>
2021-06-03 15:58:06 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -271,7 +271,7 @@ const doTheStringify = async () => {
           // console.log("This is stuffKey: ", stuffKey); // (required, type, inpType)
           // console.log("This is stuffValue: ", stuffValue); // (true, string, text)
           if (stuffKey == "inpType") {
-            // console.log("This is the stuffValue of stuffValue", stuffValue); // (text, textarea, text, array, date, number)
+            // console.log("This is the stuffValue of stuffKey", stuffValue); // (text, textarea, text, array, date, number)
             inputFunction(stuffValue);
           }
         }
