1d096426b8d01bf5051b87f987d8e2eca8a4494e
andersonriley <andersonriley28@gmail.com>
2021-06-03 08:52:14 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -273,7 +273,10 @@ function numberAttribute(vars) {
                 + '<form action="#" method="post" class="demoForm">'
                   + '<fieldset>'
                     + '<legend>Number List</legend>'
-                    + '<input type="number" id="" name="" min="0" max="10">';
+                    + '<input type="number" id="" name="" min="0" max="10">'
+                  + '</fieldset>'
+                + '</form>'
+              + '</div>';
 
   $('#numberSpot').append(numberHTML);
 }
