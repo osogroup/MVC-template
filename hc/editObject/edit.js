861785b5caf5b73bc2f970d4a71db8b3f6c33ad2
andersonriley <andersonriley28@gmail.com>
2021-06-04 10:24:14 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -451,7 +451,7 @@ function numberAttribute(num) {
                 + '<form action="#" method="post" class="demoForm">'
                   + '<fieldset class="minHeight">'
                     + '<legend>'+num.newHKey+'</legend>'
-                    + '<input type="number" id="input'+num.newHKey+'" value="'+num.newHVal+'">'
+                    + '<input type="number" id="input'+num.newHKey+'" value="'+num.newHVal+'" placeholder="'+num.newHKey+'" oninput="anyChange(this.placeholder)">'
                   + '</fieldset>'
                 + '</form>'
               + '</div>';
