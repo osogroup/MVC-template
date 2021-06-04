3ea95a8446e428ecbb0998b3986d0ed31548accb
andersonriley <andersonriley28@gmail.com>
2021-06-04 15:20:01 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -172,9 +172,9 @@ const doTheStringify = async () => {
     console.log("This is typeStuff: ", typeStuff);
 
     // filling arrayOfOptions[]
-    var tagData = data.tags;
+    var tagData = data[tags];
     console.log("This is tagData: ", tagData);
-    var nameTagData = tagData[name];
+    var nameTagData = tagData.name;
     console.log("This is nameTagData: ", nameTagData);
     for (const [tagKey, tagValue] of Object.entries(tagData)) {
       if (tagKey == 'name') {
