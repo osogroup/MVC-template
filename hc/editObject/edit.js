c4b18d796acd1ea2ddb20faf2f802ee83aece265
andersonriley <andersonriley28@gmail.com>
2021-06-04 08:27:09 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -276,7 +276,6 @@ const doTheStringify = async () => {
           }
         }
 
-
         for (const [stuffKey, stuffValue] of Object.entries(taskHeader)) {
           // console.log("This is stuffKey: ", stuffKey); // (required, type, inpType)
           // console.log("This is stuffValue: ", stuffValue); // (true, string, text)
