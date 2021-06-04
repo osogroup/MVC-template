83af1b4f31a5639773a33a361ac676811adf806f
andersonriley <andersonriley28@gmail.com>
2021-06-04 08:51:33 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -269,11 +269,13 @@ const doTheStringify = async () => {
         var taskHeader = taskStuff[headerKey];
         
         // filling up the statusOptions array before calling selectAttribute()
+        var plsBStatus = [];
         for (const [stuffKey, stuffValue] of Object.entries(taskHeader)) {
           if (stuffKey == 'opts') {
-            statusOptions.push(stuffValue);
-            console.log("This is statusOptions: ", statusOptions);
+            plsBStatus += stuffValue;
           }
+          statusOptions.push(plsBStatus);
+          console.log("This is statusOptions: ", statusOptions);
         }
 
         for (const [stuffKey, stuffValue] of Object.entries(taskHeader)) {
