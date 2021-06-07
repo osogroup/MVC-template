74bee7ef088c9642976b47fc3cd40544eb11387a
andersonriley <andersonriley28@gmail.com>
2021-06-07 08:50:00 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -317,9 +317,9 @@ const doTheStringify = async () => {
               for (const [tagTypeKey, tagTypeValue] of Object.entries(tagTypeData)) {
                 console.log("This is tagTypeKey: ", tagTypeKey);
                 console.log("This is tagTypeValue: ", tagTypeValue);
-                console.log("This is tagTypeData['id']: ", tagTypeData['id']);
+                console.log("This is tagTypeData.id: ", tagTypeData.id);
                 if (arrayOfOptions.includes(tagTypeData['id'])) {
-                  
+
                 }
               }
 
