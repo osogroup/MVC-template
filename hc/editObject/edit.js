0bce3b12561d4eeaa2e73de65a10850938cf6904
andersonriley <andersonriley28@gmail.com>
2021-06-07 15:27:11 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -378,7 +378,7 @@ function addFunction(variable) {
   console.log("This is variable: ", variable); // <input type="button" id="....
   console.log("This is the variable.id: ", variable.id); // showTxt_3
 
-  var number = $('div').attr('id').replace(/\D/g,'');
+  var number = $('div').attr('id').replace(/[^\d]/g,'');
   console.log("This is number: ", number);
 
   var select = document.getElementById('scripts');
