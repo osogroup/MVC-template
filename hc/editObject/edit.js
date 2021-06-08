543a982ad432b5fac0db5f14e36b8b4bf15b90b1
andersonriley <andersonriley28@gmail.com>
2021-06-08 08:36:30 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -3,119 +3,25 @@ $(document).ready(function(){
   doTheStringify();
 });
 
+
 // pull in data from objectList.JSON and configuration JSON file 
 const configData = async () => {
   var listConfig = await fetch("../tempData/objectConfig.json").then(response=>{return response.json();});
   return listConfig;
 }
   
-  // gets all the Objects/Arrays from listOfObjects.json and returns them
+
+// gets all the Objects/Arrays from listOfObjects.json and returns them
 const tempData = async () => {
   var objVars = await fetch("../tempData/listOfObjects.json").then(response=>{return response.json();});
   return objVars;
 }
 
+
+// gets data from GitHub according to the URL's type
 const typeData = async () => {
   var typeVars = await fetch("../tempData/"+objType+".json").then(response=>{return response.json();});
   return typeVars;
-}
-
-
-// function to create editObject HTML
-const outputHTML = async () => {
-  console.log("Entering outputHTML()");
-  var data = await tempData();
-  var config = await configData();
-  var inputBoxes = '';
-  var configTypeData = config[objType];
-  var objTypeData = data[objType];
-  // console.log("This is the objTypeData: ", objTypeData);
-  // var col = objTypeData[objItemid].length;
-  // console.log("This is the col: ", col);
-
-
-  // -------------------------------------------------- Navigation Bar --------------------------------------------------
-
-
-  var headerHTML = '<div id="contacts">'
-  + '<div class="row">'
-    + '<p id="header" class="col-12">'
-      + '<img id="imageSpacing" src="MindfulMeasuresLogo.png" alt="LogoImage" width="50">';
-  
-  // creating the links for the header
-  for(const [headerKey, headerValue] of Object.entries(data)) {
-    headerHTML += '<a class="headerLinks" href="../listObjects/?type='+headerKey+'">'+headerKey.toUpperCase()+'</a>';
-  }
-  
-  // closing header row
-  headerHTML += '</p>';
-
-  // H1 header to let the user know which object they're editing
-  headerHTML += '<h1>Edit '+objType+' Item</h1>'
-            + '</div>';
-  $('#HTMLoutput').append(headerHTML);
-
-
-  // -------------------------------------------------- Item Headers --------------------------------------------------
-
-
-  // creating items header row
-  var itemHeader = '<div class="row">';
-
-
-
-  console.log(objTypeData[objItemid]);
-
-  // This is INCORRECT, using the ID is only going to work when the ID is equal to the position in the array
-  for (const [headerKey, headerValue] of Object.entries(objTypeData[objItemid])) {
-
-    console.log("This is the objTypeData.id: ", objTypeData.id);
-    // TOGGLE THIS TO PULL INFO FROM THE LIST PAGE*************************
-    // localStorage.setItem(objType+headerKey+objTypeData.id, headerValue);
-    console.log("This is headerKey: ", headerKey);
-    console.log("This is headerValue: ", headerValue);
-    
-    // creating each object item column
-    itemHeader += '<div class="col-4 minHeight">'
-                  + '<div class="col-12">'+headerKey+'</div>'; 
-
-   
-  // -------------------------------------------------- Item Values --------------------------------------------------
-
-
-    // checking to see if the headerKey is in the editable array (in objectConfig.json)
-    if (configTypeData.editable.includes(headerKey) == true)
-    {
-      // making object item an input textbox
-      itemHeader += '<br><input class="col-12" id="input'+headerKey+'" type="textarea" value="'+localStorage.getItem(headerKey)+'" placeholder="'+headerKey+'" oninput="anyChange(this.placeholder)">';
-    }
-    else
-    {
-      // making object item a regular div
-      itemHeader += '<br><div class="col-12">'+headerValue+'</div>';
-    }
-
-    // closing each object item column
-    itemHeader += '</div>';
-  }
-  console.log(localStorage);
-  // closing item header row
-  itemHeader += '</div>';
-  $('#HTMLoutput').append(itemHeader);
-
-
-  // -------------------------------------------------- Update Button --------------------------------------------------
-
-
-  var updateButton = '<div class="row">'
-                    +  '<div class="col-11"></div>'
-                    +  '<div class="col-1"><button>Update</button></div>'
-                   + '</div>';
-   $('#HTMLoutput').append(updateButton);
-
-  //  localStorage.removeItem('');
-
-  console.log("Exiting outputHTML()");
 }
 
 
