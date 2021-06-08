8461f9cadb742d935cb99b8dacb4379158c26a5f
andersonriley <andersonriley28@gmail.com>
2021-06-08 08:44:53 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -292,11 +292,11 @@ function addFunction(variable) {
   console.log("This is thisButton: ", thisButton);
   thisButton.remove();
 
-  var newButton = '<input type="button" id="showTxt_'+variable+'" value="Add" onclick="addFunction(this)"/>';
-  console.log("This is the new button: ", newButton);
-
   var variableID = variable.id.replace(/showTxt_/, '');
   console.log("This is variableID...", variableID);
+  
+  var newButton = '<input type="button" id="showTxt_'+variableID+'" value="Add" onclick="addFunction(this)"/>';
+  console.log("This is the new button: ", newButton);
 
 
   var select = document.getElementById('scripts');
