afc9109ae15e172ad1423399d1760cd3634c15eb
andersonriley <andersonriley28@gmail.com>
2021-06-08 11:42:07 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -296,7 +296,7 @@ function addFunction(variable) {
 
   var addArray = [];
   var storageItem = objType+'_'+objItemid+'_HTMLnames';
-  var loopity = localStorage.getItem(storageItem[0]);
+  var loopity = localStorage.getItem(storageItem);
   console.log("This is loopity: ", loopity);
 
 
