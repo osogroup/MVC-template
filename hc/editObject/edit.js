2e5535ec7ec988d7767504e1f42a123f27dd51cb
andersonriley <andersonriley28@gmail.com>
2021-06-09 10:01:32 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -636,7 +636,7 @@ function arrayList(array) {
                         + '<div id="appendTo">';
 
   // if objectKeyNames_name exists in localStorage {
-    if (localStorage.getItem(objectKeyNames != null)) {
+    if (localStorage.getItem(objectKeyNames) != null) {
       // pull from localStorage
       itemValueNames = localStorage.getItem(objectKeyNames);
       console.log("This is itemValueNames: ", itemValueNames);
