8a19fd53c1696998fa7e79ba0c1d94520394dd1d
andersonriley <andersonriley28@gmail.com>
2021-06-09 07:17:30 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -321,7 +321,7 @@ function addFunction(variable) {
   // get entire button
 
   // change input's id
-  variable.id.prop('id', 'showTxt_1000');
+  variable.id = 'showTxt_1000';
 
   // create a new button with incremented value
   // var newButton = '<input type="button" id="showTxt_'+variableID+'" value="Add" onclick="addFunction(this)"/>';
