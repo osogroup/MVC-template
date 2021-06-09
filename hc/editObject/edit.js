88c9f6b8a264343a1a80dd1240a270b22ff9dae6
andersonriley <andersonriley28@gmail.com>
2021-06-09 13:19:27 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -360,6 +360,8 @@ function addFunction(variable) {
     console.log("This is the new tagArray: ", tagArray);
 
     superObjLoop.tags = JSON.stringify(tagArray);
+
+    console.log("This is superObjLoop: ", superObjLoop);
   
     // adding a new row and columns to the HTML
     var HTMLelement = '<div class="row"><div class="col-10">'+elementVal+'</div><div class="col-2"><input type="button" id="remvBtn_'+variableID+'" value="-" onclick="removeFunction('+variableID+')"></div></div>';
