feb080340b307495074cb28146b90ebc313895cd
andersonriley <andersonriley28@gmail.com>
2021-06-09 15:19:10 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -308,9 +308,10 @@ function addFunction(variable) {
   console.log("This is elementVal: ", elementIDNum);
   
   // use this value (0/1/2) from elementVal to add the values to task_0.tag
+  console.log("This is JSON.parse(localStorage.getItem(objType+'_'+objItemid)).tags[elementIDNum]: ", JSON.parse(localStorage.getItem(objType+'_'+objItemid)).tags[elementIDNum])
+  
   var HTMLelement = '<div class="row"><div class="col-10">'+JSON.parse(localStorage.getItem(objType+'_'+objItemid)).tags[elementIDNum]+'</div><div class="col-2"><input type="button" id="remvBtn_'+elementIDNum+'" value="-" onclick="removeFunction('+elementIDNum+')"></div></div>';
 
-  console.log("This is JSON.parse(localStorage.getItem(objType+'_'+objItemid)).tags[elementIDNum]: ", JSON.parse(localStorage.getItem(objType+'_'+objItemid)).tags[elementIDNum])
 
 
 }
