f63e37258d7b5956a98ad419c14eeeb73fc5250e
andersonriley <andersonriley28@gmail.com>
2021-06-10 07:42:23 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -595,6 +595,9 @@ function arrayList(array) {
   for (const [arrayKey, arrayValue] of Object.entries(newDataArray)) {
     console.log("This is arrayKey: ", arrayKey);
     console.log("This is arrayValue: ", arrayValue);
+    if (tagArray.includes(arrayKey)) {
+      console.log("tagArray includes", arrayKey);
+    }
   }
 
 
