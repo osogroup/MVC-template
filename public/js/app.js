1f89401d0be8f88925a586af9bdaf810b7d25b8b
andersonriley <andersonriley28@gmail.com>
2021-06-11 11:37:48 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,13 @@
 let auth0 = null;
 
 const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configureClient = async () => {
+    const response = await fetchAuthConfig();
+    const config = await response.json();
+  
+    auth0 = await createAuth0Client({
+      domain: config.domain,
+      client_id: config.clientId
+    });
+  };
