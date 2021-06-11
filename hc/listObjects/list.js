0d2a759e46a8083518c99636b0e418e587de3e77
andersonriley <andersonriley28@gmail.com>
2021-06-11 15:13:02 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -16,29 +16,71 @@ const tempData = async () => {
   return objVars;
 }
 
+let auth0 = null;
+
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
+
+window.onload = async () => {
+  await configureClient();
+
+  updateUI();
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    // show the gated content
+    return;
+  }
+
+  // NEW - check for the code and state parameters
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+
+    // Process the login state
+    await auth0.handleRedirectCallback();
+    
+    updateUI();
+
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
+};
+
+const updateUI = async () => {
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-login").disabled = isAuthenticated;
+
+  if (isAuthenticated) {
+    document.getElementById("gated-content").classList.remove("hidden");
+
+    document.getElementById(
+      "ipt-access-token"
+    ).innerHTML = await auth0.getTokenSilently();
+
+    document.getElementById("ipt-user-profile").textContent = JSON.stringify(
+      await auth0.getUser()
+    );
+
+  } else {
+    document.getElementById("gated-content").classList.add("hidden");
+  }
+};
+
 // gathers, sorts, and organizes all the Objects and data from the JSON files, generates HTML, and appends it to #TitleOfList in index.html
 const createHTML = async () => {
-
-  let auth0 = null;
-
-  const fetchAuthConfig = () => fetch("/auth_config.json");
-
-  const configureClient = async () => {
-    const response = await fetchAuthConfig();
-    const config = await response.json();
-
-    auth0 = await createAuth0Client({
-      domain: config.domain,
-      client_id: config.clientId
-    });
-  };
-
-  const logout = () => {
-    auth0.logout({
-      returnTo: window.location.origin
-    });
-  };
-
+  
   console.log("Entering createHTML()");
   var data = await tempData();
   var config = await configData();
