96f007f4391b902d76d855e49ccd548f0e1cf4fc
andersonriley <andersonriley28@gmail.com>
2021-06-11 15:19:21 -0500

    Update list.js

--- hc/listObjects/list.js
+++ hc/listObjects/list.js
@@ -38,7 +38,7 @@ const createHTML = async () => {
     headerHTML            +='<a class="headerLinks" href="/hc/listObjects/?type=' + headerKey + '">' + headerKey.toUpperCase() + '</a>';
   }
 
-  headerHTML              +='<a href="#" class="headerLinks" id="btn-logout" onclick="logout()">Logout</a>'
+  headerHTML              +='<a href="#" class="headerLinks" id="btn-logout" onclick="logout()">LOGOUT</a>'
                         + '</p>'
                       + '</div>'
                     + '<h1>OAS Object List</h1>'
