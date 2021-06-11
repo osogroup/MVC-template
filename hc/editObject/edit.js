ec6bfe96b77a2ece39e9054e98d40ed9f145b1fd
andersonriley <andersonriley28@gmail.com>
2021-06-11 09:20:12 -0500

    Update edit.js

--- hc/editObject/edit.js
+++ hc/editObject/edit.js
@@ -622,7 +622,12 @@ function arrayList(array) {
 
   // creating all the options from the arrayOfOptions array in the select tag
   for (const [optionKey, optionValue] of Object.entries(array.newData.tags)) {
-    HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'">'+optionValue.name+'</option>';
+    if (tagObj.includes(optionValue)) {
+      HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'">'+optionValue.name+'</option>';
+    }
+     else {
+      HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'" selected>'+optionValue.name+'</option>';
+     } 
   }
 
   // closing the form tags and creating the add button
