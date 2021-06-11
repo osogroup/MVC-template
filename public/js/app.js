e59c589ec8d85c6b0c3599363ed20ace0dd853e3
andersonriley <andersonriley28@gmail.com>
2021-06-11 11:43:14 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,13 @@ let auth0 = null;
 const fetchAuthConfig = () => fetch("/auth_config.json");
 
 const configureClient = async () => {
-    const response = await fetchAuthConfig();
-    const config = await response.json();
-  
-    auth0 = await createAuth0Client({
-      domain: config.domain,
-      client_id: config.clientId
-    });
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
   };
 
   window.onload = async () => {
@@ -23,4 +23,10 @@ const configureClient = async () => {
   
     document.getElementById("btn-logout").disabled = !isAuthenticated;
     document.getElementById("btn-login").disabled = isAuthenticated;
+  };
+
+  const login = async () => {
+    await auth0.loginWithRedirect({
+      redirect_uri: window.location.origin
+    });
   };
