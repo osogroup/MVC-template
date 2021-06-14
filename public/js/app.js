0ea0c0e44981313cf80955e3ba2142a2e81cf499
andersonriley <andersonriley28@gmail.com>
2021-06-14 11:30:54 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -63,7 +63,7 @@ const updateUI = async () => {
 
 const login = async () => {
   await auth0.loginWithRedirect({
-    redirect_uri: 'http://oso.group.s3-website-us-east-1.amazonaws.com/hc/listObjects/'
+    redirect_uri: 'http://oso.group.s3-website-us-east-1.amazonaws.com/hc/listObjects/?type=task'
   });
 };
 
