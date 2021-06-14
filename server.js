8541b72f332f396e7be5596ddd4462e8baac5206
andersonriley <andersonriley28@gmail.com>
2021-06-14 10:02:06 -0500

    install express packages

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 const { join } = require("path");
+const authConfig = require("./auth_config.json");
+
 const app = express();
 
 // Serve static assets from the /public folder
@@ -17,3 +21,31 @@ app.get("/*", (_, res) => {
 
 // Listen on port 3000
 app.listen(3000, () => console.log("Application running on port 3000"));
+
+// create the JWT middleware
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithms: ["RS256"]
+});
+
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
+});
