afeea4479b1b1a9c77ee0576059a40f5026c23b5
andersonriley <andersonriley28@gmail.com>
2021-06-14 13:16:31 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -88,9 +88,14 @@ const createHTML = async () => {
     HTMLoutput += '<div class="col-4">'
                   + '<div class="col-12">'+editableValue+'</div>';
 
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
     // console.log("This is type[editableValue].type", type[editableValue].type);
     inputFunction(type[editableValue].type);
     
+
     // if the editableValue is in the required list according to objType 
     if (configTypeData.required.includes(editableValue) == true) {
       // make the input box with a "required" attribute (includes '*' to let the admin see the required fields)
