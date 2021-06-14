7d343784e91edb2d57488efd27d50aa06acbb536
andersonriley <andersonriley28@gmail.com>
2021-06-14 08:23:21 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -40,6 +40,7 @@ window.onload = async () => {
 
 const updateUI = async () => {
   const isAuthenticated = await auth0.isAuthenticated();
+  console.log("isAuthenticated: ", isAuthenticated);
 
   document.getElementById("btn-logout").disabled = !isAuthenticated;
   document.getElementById("btn-login").disabled = isAuthenticated;
