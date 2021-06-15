87651e3907c92f136503fe4a6807ea29e937125b
andersonriley <andersonriley28@gmail.com>
2021-06-15 11:40:42 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -126,11 +126,11 @@ const createHTML = async () => {
     if (type[editableValue].inpType == 'array') {
       var arrayArray = type[editableValue].opts;
       console.log("This is arrayArray:",arrayArray);
-      
+
       var tagNames = {};
       // for loop putting all the tag names into an array according to the values in arrayArray
       for (const [arrayKey, arrayValue] of Object.entries(arrayArray)) {
-        console.log("This is data['tags']:",data['tags'])
+        console.log("This is data['tags']:",data['tags'][arrayValue])
         // if (data['tags'].id == arrayValue) {
         //   console.log("YES");
         // }
