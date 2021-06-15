497db231dc1bfa3b942c3aa6db4d5c6d8abd2513
andersonriley <andersonriley28@gmail.com>
2021-06-15 07:27:26 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -41,7 +41,7 @@ function anyChange(str) {
   }
   var changeValue = str.value;
   console.log("This is change:", changeValue);
-  var objKey = superKey.replace(/task_21/, '');
+  var objKey = str.id.replace(/task_21/, '');
   console.log("This is objKey:", objKey);
   // myObj[objKey] = changeValue;
   // console.log("this is myObject[str]", myObject[str]);
