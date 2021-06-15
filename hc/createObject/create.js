d5cfc1a1a937e3753ff2590815f2f3968f712c40
andersonriley <andersonriley28@gmail.com>
2021-06-15 13:44:01 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -388,16 +388,15 @@ function arrayList(array) {
                     + '</div>'
                     + '<br><select id="scripts" name="scripts">';
 
-  // // creating all the options from the arrayOfOptions array in the select tag
-  // for (const [optionKey, optionValue] of Object.entries(array.newData.tags)) {
-  //   if (optionKey in tagObj) {
-  //     HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'">'+optionValue.name+'</option>';
-  //   }
-  //   else {
-  //     HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'" selected>'+optionValue.name+'</option>';
-  //   }
-    
-  // }
+  // creating all the options from the arrayOfOptions array in the select tag
+  for (const [optionKey, optionValue] of Object.entries(array.newData.tags)) {
+    if (optionKey in tagObj) {
+      HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'">'+optionValue.name+'</option>';
+    }
+    else {
+      HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'" selected>'+optionValue.name+'</option>';
+    }
+  }
 
   // closing the form tags and creating the add button
   HTMLoutput          +='</select>'
