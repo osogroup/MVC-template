6d3bc5606ffff5d53d7b6c9fec4c9e74b472129f
andersonriley <andersonriley28@gmail.com>
2021-06-15 11:58:53 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -132,7 +132,7 @@ const createHTML = async () => {
       for (const [arrayKey, arrayValue] of Object.entries(data['tags'])) {
         // console.log("id of 0, 1, and 2 pls",data['tags'][arrayValue].id)
         if (arrayArray.includes(data['tags'][arrayValue].id)) {
-          console.log("YES");
+          console.log(data['tags'][arrayValue].name);
         }
       }
     }
