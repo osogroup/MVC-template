9bdb01450ef81da24d89f7bd45ae8c1779a8ea6c
andersonriley <andersonriley28@gmail.com>
2021-06-15 14:35:55 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -384,20 +384,20 @@ function addFunction(addBtn) {
   // getting the number from the id of the selected option
   var elementVal = select.options[select.selectedIndex].id.replace(/optionValue_/, ''); // 0/1/2
 
-  tagList = JSON.parse(localStorage.getItem(objTypeID));
+  tagList = JSON.parse(localStorage.getItem(objTypeID['tags']));
   console.log("This is tagList: ", tagList);
 
-  // if (tagList.includes(Number(elementVal))) {
-  //   console.log("tagList already includes", elementVal);
-  // }
-  // else {
+  if (tagList.tags.includes(Number(elementVal))) {
+    console.log("tagList already includes", elementVal);
+  }
+  else {
   //   tagList.push(Number(elementVal));
   //   tagList.sort();
   //   JSON.stringify(tagList);
   //   localObj.tags = tagList;
   //   console.log("This is the localObj with updated tags: ", localObj);
   //   localStorage.setItem(objTypeID, JSON.stringify(localObj));
-  // }
+  }
   // location.reload();
 }
 
