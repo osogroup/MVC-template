d884e3dfdcc07fdb18a3116f1f7e063026aaaf69
andersonriley <andersonriley28@gmail.com>
2021-06-15 07:49:52 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -41,7 +41,7 @@ function anyChange(str) {
 
   var myStr = localStorage.getItem(superKey);
   console.log("This is myStr:", myStr);
-  if (myObj == null || myObj == " ") {
+  if (myStr == null || myStr == " ") {
     console.log("myStr is empty");
     var myObj = myStr;
   }
