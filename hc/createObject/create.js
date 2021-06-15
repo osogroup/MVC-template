71183827fc6b20da58e524e68ee62e07ea734e14
andersonriley <andersonriley28@gmail.com>
2021-06-15 09:44:46 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -284,7 +284,7 @@ function calendarAttribute(cal) {
                   + '<form action="#" method="post" class="demoForm">'
                     + '<fieldset class="minHeight">'
                       + '<legend>'+cal.value+'</legend>'
-                      + '<input type="date" id="calInput" name="">'
+                      + '<input type="date" id="'+cal.temp+'_'+cal.value+'" name="">'
                     + '</fieldset>'
                   + '</form>'
                 + '</div>';
