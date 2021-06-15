7f1f2b14627675f1077c1ae3525ac1b38890a852
andersonriley <andersonriley28@gmail.com>
2021-06-15 11:54:49 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -130,7 +130,7 @@ const createHTML = async () => {
       var tagNames = {};
       // for loop putting all the tag names into an array according to the values in arrayArray
       for (const [arrayKey, arrayValue] of Object.entries(arrayArray)) {
-        console.log("id pls",data['tags'][arrayValue].id)
+        console.log("id of 0, 1, and 2 pls",data['tags'][arrayValue].id)
         // if (data['tags'].id == arrayValue) {
         //   console.log("YES");
         // }
