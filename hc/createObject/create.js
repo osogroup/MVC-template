d1cf5d6f1e07d19e62f6e8f162537070c3abf691
andersonriley <andersonriley28@gmail.com>
2021-06-15 11:05:39 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -350,48 +350,48 @@ function arrayList(array) {
                         + '<div id="appendTo">';
 
   // fill up array, tagNumbers, with tag numbers
-  for (const [arrayKey, arrayValue] of Object.entries(JSON.parse(localStorage.getItem(objTypeID)))) {
-    // arrayKey: id, name, description, status, ...
-    // arrayValue: 1, COI: Static Site HTML Structure, This task creates the structure of the Static site, ...
-    if (arrayKey == 'tags') {
-      for (const [tagKey, tagValue] of Object.entries(arrayValue)) {
-        tagNumbers.push(tagValue);
-      }
-    }
-  }
+  // for (const [arrayKey, arrayValue] of Object.entries(JSON.parse(localStorage.getItem(objTypeID)))) {
+  //   // arrayKey: id, name, description, status, ...
+  //   // arrayValue: 1, COI: Static Site HTML Structure, This task creates the structure of the Static site, ...
+  //   if (arrayKey == 'tags') {
+  //     for (const [tagKey, tagValue] of Object.entries(arrayValue)) {
+  //       tagNumbers.push(tagValue);
+  //     }
+  //   }
+  // }
 
-  // if tag number array includes the tag number, display the tag name
-  for (const [arrayKey, arrayValue] of Object.entries(array.newData.tags)) {
-    if (tagNumbers.includes(arrayValue.id)) {
-      tagNames.push(arrayValue.name);
-    }
-  }
+  // // if tag number array includes the tag number, display the tag name
+  // for (const [arrayKey, arrayValue] of Object.entries(array.newData.tags)) {
+  //   if (tagNumbers.includes(arrayValue.id)) {
+  //     tagNames.push(arrayValue.name);
+  //   }
+  // }
 
-  // filling tagObj with two arrays: tagNumbers and tagNames
-  tagNumbers.forEach((key, i) => tagObj[key] = tagNames[i]);
+  // // filling tagObj with two arrays: tagNumbers and tagNames
+  // tagNumbers.forEach((key, i) => tagObj[key] = tagNames[i]);
 
-  // showing all the items in the arrayOfOptionsNames array (none if the array is preset as empty)
-  for (const[arrayKey, arrayValue] of Object.entries(tagObj)) {
-    HTMLarrayValues       +='<div class="row"><div class="col-10">'+arrayValue+'</div><div class="col-2"><input type="button" id="remvBtn_'+arrayKey+'" value="-" onclick="removeFunction(this)"></div></div>';
-    // counts up the indices if there's any preset values in the array
-  }
+  // // showing all the items in the arrayOfOptionsNames array (none if the array is preset as empty)
+  // for (const[arrayKey, arrayValue] of Object.entries(tagObj)) {
+  //   HTMLarrayValues       +='<div class="row"><div class="col-10">'+arrayValue+'</div><div class="col-2"><input type="button" id="remvBtn_'+arrayKey+'" value="-" onclick="removeFunction(this)"></div></div>';
+  //   // counts up the indices if there's any preset values in the array
+  // }
   
-  // creating the select tag
-  HTMLoutput += HTMLarrayValues
-                        + '</div>'
-                      + '</div>'
-                      + '<br><select id="scripts" name="scripts">';
+  // // creating the select tag
+  // HTMLoutput += HTMLarrayValues
+  //                       + '</div>'
+  //                     + '</div>'
+  //                     + '<br><select id="scripts" name="scripts">';
 
-  // creating all the options from the arrayOfOptions array in the select tag
-  for (const [optionKey, optionValue] of Object.entries(array.newData.tags)) {
-    if (optionKey in tagObj) {
-      HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'">'+optionValue.name+'</option>';
-    }
-    else {
-      HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'" selected>'+optionValue.name+'</option>';
-    }
+  // // creating all the options from the arrayOfOptions array in the select tag
+  // for (const [optionKey, optionValue] of Object.entries(array.newData.tags)) {
+  //   if (optionKey in tagObj) {
+  //     HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'">'+optionValue.name+'</option>';
+  //   }
+  //   else {
+  //     HTMLoutput          +='<option id="optionValue_'+optionValue.id+'" value="'+optionValue.name+'" selected>'+optionValue.name+'</option>';
+  //   }
     
-  }
+  // }
 
   // closing the form tags and creating the add button
   HTMLoutput          +='</select>'
