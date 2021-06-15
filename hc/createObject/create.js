0e2c503b72013b9517d62f7af8098dcb8d985e6b
andersonriley <andersonriley28@gmail.com>
2021-06-15 10:51:12 -0500

    Update create.js

--- hc/createObject/create.js
+++ hc/createObject/create.js
@@ -304,7 +304,7 @@ function selectAttribute(sel) {
                   + '<fieldset class="minHeight">'
                     + '<legend>'+sel.value+'</legend>'
                     + '<select id="'+sel.temp+'_'+sel.value+'" name="optionDisp" value="optionDisp" oninput="selectedOption(this)">';
-
+                      + '<option value="Select an Option">Select an Option</option>';
   for (const [varsKey, varsValue] of Object.entries(sel.options)) {
     selectHTML +=       '<option value="'+varsValue+'">'+varsValue+'</option>';
   }
