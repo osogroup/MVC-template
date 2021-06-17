c8acfd3a10b2f4917a41fd2dc608ca681d5341eb
andersonriley <andersonriley28@gmail.com>
2021-06-17 07:37:15 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -57,20 +57,25 @@ const updateUI = async () => {
   }
   else if (isAuthenticated && URLValue == 'create') {  
     console.log("URLValue is create");
+    document.getElementById("gated-content-1").classList.add("hidden");
     document.getElementById("gated-content-2").classList.add("hidden");
-  }
+    document.getElementById("gated-content-3").classList.add("hidden");
+    document.getElementById("gated-content-4").classList.remove("hidden");  }
   else if (isAuthenticated) {
-    document.getElementById("gated-content-1").classList.remove("hidden");
+    document.getElementById("gated-content-1").classList.add("hidden");
     document.getElementById("gated-content-2").classList.add("hidden");
+    document.getElementById("gated-content-3").classList.remove("hidden");
+    document.getElementById("gated-content-4").classList.add("hidden");
 
     // document.getElementById("ipt-access-token").innerHTML = await auth0.getTokenSilently();
-
     // document.getElementById("ipt-user-profile").textContent = JSON.stringify(await auth0.getUser());
 
   }
   else {
     document.getElementById("gated-content-1").classList.add("hidden");
-    document.getElementById("gated-content-2").classList.remove("hidden");
+    document.getElementById("gated-content-2").classList.add("hidden");
+    document.getElementById("gated-content-3").classList.remove("hidden");
+    document.getElementById("gated-content-4").classList.add("hidden");
     
   }
 };
