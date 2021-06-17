a5e654b0e4537fd8152894bcb487dc2c8d8b6c0b
andersonriley <andersonriley28@gmail.com>
2021-06-17 10:41:50 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -44,7 +44,7 @@ const updateUI = async () => {
   const isAuthenticated = await auth0.isAuthenticated();
   console.log("isAuthenticated?:", isAuthenticated);
 
-  var name = await auth0.getUser();
+  var name = JSON.stringify(await auth0.getUser());
   document.getElementById("ipt-user-profile").textContent = name;
   document.getElementById("btn-logout").hidden = !isAuthenticated;
   document.getElementById("btn-login").hidden = isAuthenticated;
