10a03956d10c187087a968e4698a089b7fd98c66
andersonriley <andersonriley28@gmail.com>
2021-06-17 08:12:53 -0500

    Update create.js

--- public/hc/createObject/create.js
+++ public/hc/createObject/create.js
@@ -78,7 +78,7 @@ const createHTML = async () => {
   // creating nav bar row
                   + '<div class="row">'
                     + '<p id="header">'
-                      + '<img id="imageSpacing" src="MindfulMeasuresLogo.png" alt="LogoImage" width="80">';
+                      + '<img id="imageSpacing" src="images/MindfulMeasuresLogo.png" alt="LogoImage" width="80">';
   
   // creating the links for the header
   for(const [headerKey, headerValue] of Object.entries(data)) {
