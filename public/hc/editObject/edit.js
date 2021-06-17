1b32355cc334abaa55bf96a4d0b4884dd07dbdf3
andersonriley <andersonriley28@gmail.com>
2021-06-17 09:21:40 -0500

    Update edit.js

--- public/hc/editObject/edit.js
+++ public/hc/editObject/edit.js
@@ -193,9 +193,7 @@ const doTheStrings = async () => {
 
     // --------------------------------------- Item Header and Inputs ---------------------------------------
 
-    //
-    // creating item header row ((((((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))))))))
-    //
+    // (((((((((((((((((((((((((((((((((((((creating item header row)))))))))))))))))))))))))))))))))))))
     HTMLoutput += '<div class="row">'; 
     // create HTML header and fields
     for (const [headerKey, headerValue] of Object.entries(forOfLoop)) {
