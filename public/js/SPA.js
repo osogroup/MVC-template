f001af17bfc6f2e00c8ac8140ac9ac63d9cb81df
andersonriley <andersonriley28@gmail.com>
2021-06-28 07:44:34 -0500

    Update SPA.js

--- public/js/SPA.js
+++ public/js/SPA.js
@@ -88,10 +88,10 @@ const generateID = async () => {
 
 // gathers, sorts, and organizes all the Objects and data from the JSON files, generates HTML, and appends it to #TitleOfList in index.html
 const HTMLGenerate = async () => {
-  if (!objType) {
-    var typeButton = '<a href="/?type=task">task</a>';
-    $('TitleOfList').append(typeButton);
-  }
+  // if (!objType) {
+  //   var typeButton = '<a href="/?type=task">task</a>';
+  //   $('TitleOfList').append(typeButton);
+  // }
   
   console.log("Entering HTMLGenerate()");
   var data = await tempData();
