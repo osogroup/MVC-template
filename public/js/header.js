70cdab4f2d98cd70738ca3dbf5670836ea5afd1b
andersonriley <andersonriley28@gmail.com>
2021-07-01 14:11:52 -0500

    Update header.js

--- public/js/header.js
+++ public/js/header.js
@@ -1,16 +1,24 @@
-var HTMLoutput = '<div id="contacts">'
-                 + '<div class="row">'
-                   + '<p id="header">'
-                     + '<img id="imageSpacing" src="images/MindfulMeasuresLogo.png" alt="LogoImage" width="80">';
+$(document).ready(function () {
+  navBarGenerate();
+});
 
-// creating the links for the header
-for(const [headerKey, headerValue] of Object.entries(data)) {
-  HTMLoutput +=         '<a class="headerLinks" href="/?type='+headerKey+'&value=list">'+headerKey.toUpperCase()+'</a>';
+
+const navBarGenerate = async () => {
+  var data = await tempData();
+  var HTMLoutput = '<div id="contacts">'
+                  + '<div class="row">'
+                    + '<p id="header">'
+                      + '<img id="imageSpacing" src="images/MindfulMeasuresLogo.png" alt="LogoImage" width="80">';
+
+  // creating the links for the header
+  for(const [headerKey, headerValue] of Object.entries(data)) {
+    HTMLoutput +=         '<a class="headerLinks" href="/?type='+headerKey+'&value=list">'+headerKey.toUpperCase()+'</a>';
+  }
+
+  // closing header row
+  HTMLoutput +=         '</p>'                 
+                    + '</div>'
+                  + '</div>';
+
+  $('#navBar').append(HTMLoutput);
 }
-
-// closing header row
-HTMLoutput +=         '</p>'                 
-                  + '</div>'
-                + '</div>';
-
-$('#navBar').append(HTMLoutput);
